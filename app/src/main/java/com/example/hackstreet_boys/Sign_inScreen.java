af89a0a1f1853ddcab6f7f7d6f053efc604e667d
Garrett <gavosecky@gmail.com>
2025-03-07 15:18:10 -0600

    Added forgot password button, which changes between Sign In screen to Forgot Password screen.

--- app/src/main/java/com/example/hackstreet_boys/Sign_inScreen.java
+++ app/src/main/java/com/example/hackstreet_boys/Sign_inScreen.java
@@ -1,5 +1,6 @@
 package com.example.hackstreet_boys;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +9,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Sign_inScreen extends AppCompatActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
+
+public class Sign_inScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_in_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.sign_in_screen);
+
+        // Get references to UI elements
+        //TextView textView = findViewById(R.id.textView);
+        Button button = findViewById(R.id.forgotPassButton);
+
+        // Set button click event
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start SecondActivity
+                Intent intent = new Intent(Sign_inScreen.this, forget_passwordScreen.class);
+                startActivity(intent);
+            }
         });
     }
 }
