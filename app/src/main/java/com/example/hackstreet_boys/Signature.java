63f2e35fda47b1b1205053c6c4e2abe02572960d
Jeremy Tarvin <jeremy.tarvin@gmail.com>
2025-03-24 15:21:12 -0500

    Hardly any progress but I've recently found a neat solution to adding views: https://stackoverflow.com/questions/2395769/how-to-programmatically-add-views-to-views

---
+++ app/src/main/java/com/example/hackstreet_boys/Signature.java
@@ -0,0 +1,18 @@
+package com.example.hackstreet_boys;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Signature extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.signature);
+    }
+}
