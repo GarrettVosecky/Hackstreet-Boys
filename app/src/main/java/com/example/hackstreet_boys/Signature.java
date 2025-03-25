d1448be226f96aa78db2803aa797ce41ec203896
Jeremy Tarvin <jeremy.tarvin@gmail.com>
2025-03-25 15:20:50 -0500

    Some progress on drawing

--- app/src/main/java/com/example/hackstreet_boys/Signature.java
+++ app/src/main/java/com/example/hackstreet_boys/Signature.java
@@ -2,6 +2,8 @@ package com.example.hackstreet_boys;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,5 +16,33 @@ public class Signature extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.signature);
+        View SignaturePanel = findViewById(R.id.panel);
+        SignaturePanel.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int x = (int) event.getX();
+                int y = (int) event.getY();
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        Log.d("TAG", "touched down");
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        Log.d("TAG", "moving: (" + x + ", " + y + ")");
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        Log.d("TAG", "touched up");
+                        break;
+                }
+
+
+
+                return true;
+            }
+        });
+        {
+
+        };
+
     }
 }
