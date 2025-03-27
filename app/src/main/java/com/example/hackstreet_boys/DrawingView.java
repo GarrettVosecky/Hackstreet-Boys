37762dee9464432479ca44215d61d4c86cacffd8
Jeremy Tarvin <jeremy.tarvin@gmail.com>
2025-03-27 15:21:40 -0500

    1

---
+++ app/src/main/java/com/example/hackstreet_boys/DrawingView.java
@@ -0,0 +1,55 @@
+package com.example.hackstreet_boys;
+
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.graphics.*;
+import android.content.Context;
+
+public class DrawingView extends View {
+
+    private Path drawPath;
+    private Paint drawPaint;
+    private Paint canvasPaint;
+    private Canvas drawCanvas;
+
+    public DrawingView(Context context) {
+        super(context);
+        setupDrawing();
+    }
+
+    private void setupDrawing() {
+        drawPath = new Path();
+        drawPaint = new Paint();
+        drawPaint.setColor(Color.BLACK);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(5);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                drawPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX, touchY);
+                Log.d("Tag", touchX + " " + touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+}
--- app/src/main/java/com/example/hackstreet_boys/Signature.java
+++ app/src/main/java/com/example/hackstreet_boys/Signature.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.graphics.*;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,21 +18,33 @@ public class Signature extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.signature);
         View SignaturePanel = findViewById(R.id.panel);
+        Path drawPath = new Path();
+        Canvas drawCanvas = new Canvas();
+
+        Paint drawPaint = new Paint();
+        drawPaint.setColor(Color.BLACK);
+        drawPaint.setStrokeWidth(5);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+
         SignaturePanel.setOnTouchListener(new View.OnTouchListener() {
+            int LastX;
+            int LastY;
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 int x = (int) event.getX();
                 int y = (int) event.getY();
-
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
-                        Log.d("TAG", "touched down");
+
                         break;
                     case MotionEvent.ACTION_MOVE:
-                        Log.d("TAG", "moving: (" + x + ", " + y + ")");
+                        drawCanvas.drawLine(LastX, LastY, x, y, drawPaint);
+                        Log.d("Tag", x + " " + y);
                         break;
                     case MotionEvent.ACTION_UP:
-                        Log.d("TAG", "touched up");
+
                         break;
                 }
 
