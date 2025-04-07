a3a3b35233915d9629258ef91a207c8b4229e83b
149250 noah <149250@g.bettendorf.k12.ia.us>
2025-04-07 15:18:28 -0500

    current jobs page is blank with a way to go to completed jobs page

--- app/src/main/java/com/example/hackstreet_boys/CurrentJobs.java
+++ app/src/main/java/com/example/hackstreet_boys/CurrentJobs.java
@@ -1,7 +1,8 @@
 package com.example.hackstreet_boys;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -20,5 +21,14 @@ public class CurrentJobs extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Button btn = findViewById(R.id.completedjobs);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(getApplicationContext(), CompletedJobsPage.class);
+                startActivity(myIntent);
+            }
+        });
+
     }
 }
