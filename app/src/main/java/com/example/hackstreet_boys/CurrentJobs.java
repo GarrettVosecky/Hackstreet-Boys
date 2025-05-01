f4fdb0c8603817b220c3a3f6fdf2ebd4710e6d8f
149250 noah <149250@g.bettendorf.k12.ia.us>
2025-05-01 14:08:00 -0500

    current jobs add fragment import

--- app/src/main/java/com/example/hackstreet_boys/CurrentJobs.java
+++ app/src/main/java/com/example/hackstreet_boys/CurrentJobs.java
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.example.hackstreet_boys.AddFragment;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
